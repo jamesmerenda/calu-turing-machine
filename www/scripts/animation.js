113cdd9bce8e7318b377893c6fceb70e34ae9879
James-Merenda <jmerenda45@gmail.com>
2021-03-02 20:23:01 -0500

    reworked how cells and contents are displayed and animated.

--- www/scripts/animation.js
+++ www/scripts/animation.js
@@ -1,20 +1,47 @@
-var cell = [document.getElementById('0'),
-			document.getElementById('1'),
-			document.getElementById('2'),
-			document.getElementById('3'),
-			document.getElementById('4'),
-			document.getElementById('5'),
-			document.getElementById('6'),
-			document.getElementById('7'),
-			document.getElementById('8'),
-			document.getElementById('9'),
-			document.getElementById('10')];
+var cell = [document.getElementById('0'),document.getElementById('1'),document.getElementById('2'),
+			document.getElementById('3'),document.getElementById('4'),document.getElementById('5'),
+			document.getElementById('6'),document.getElementById('7'),document.getElementById('8'),
+			document.getElementById('9'),document.getElementById('10'),document.getElementById('11'),
+			document.getElementById('12'),document.getElementById('13'),document.getElementById('14'),
+			document.getElementById('15'),document.getElementById('16'),document.getElementById('17'),
+			document.getElementById('18'),document.getElementById('19'),document.getElementById('20'),
+			document.getElementById('21'),document.getElementById('22'),document.getElementById('23'),
+			document.getElementById('24'),document.getElementById('25'),document.getElementById('26'),
+			document.getElementById('27'),document.getElementById('28'),document.getElementById('29'),
+			document.getElementById('30'),document.getElementById('31'),document.getElementById('32'),
+			document.getElementById('33'),document.getElementById('34'),document.getElementById('35'),
+			document.getElementById('36'),document.getElementById('37'),document.getElementById('38'),
+			document.getElementById('39'),document.getElementById('40'),document.getElementById('41'),
+			document.getElementById('42'),document.getElementById('43'),document.getElementById('44'),
+			document.getElementById('45'),document.getElementById('46'),document.getElementById('47'),
+			document.getElementById('48'),document.getElementById('49'),document.getElementById('50'),
+				document.getElementById('51'),document.getElementById('52'),
+			document.getElementById('53'),document.getElementById('54'),document.getElementById('55'),
+			document.getElementById('56'),document.getElementById('57'),document.getElementById('58'),
+			document.getElementById('59'),document.getElementById('60'),document.getElementById('61'),
+			document.getElementById('62'),document.getElementById('63'),document.getElementById('64'),
+			document.getElementById('65'),document.getElementById('66'),document.getElementById('67'),
+			document.getElementById('68'),document.getElementById('69'),document.getElementById('70'),
+			document.getElementById('71'),document.getElementById('72'),document.getElementById('73'),
+			document.getElementById('74'),document.getElementById('75'),document.getElementById('76'),
+			document.getElementById('77'),document.getElementById('78'),document.getElementById('79'),
+			document.getElementById('80'),document.getElementById('81'),document.getElementById('82'),
+			document.getElementById('83'),document.getElementById('84'),document.getElementById('85'),
+			document.getElementById('86'),document.getElementById('87'),document.getElementById('88'),
+			document.getElementById('89'),document.getElementById('90'),document.getElementById('91'),
+			document.getElementById('92'),document.getElementById('93'),document.getElementById('94'),
+			document.getElementById('95'),document.getElementById('96'),document.getElementById('97'),
+			document.getElementById('98'),document.getElementById('99'),document.getElementById('100'),
+			];
 
 var basecellpos = [];
 var cellpos = [];
+var tapeBeginning = findBeginning();
+var tapeEnd = findEnd();
 
 var speedSelector = document.getElementById("speedSelector");
 var desiredSpeed = parseInt(speedSelector.value)/100 + 's';
+var displaySpeed = desiredSpeed;
 
 for(let i=0;i<cell.length;i++)
 {
@@ -22,49 +49,47 @@ for(let i=0;i<cell.length;i++)
 	cellpos[i] = parseFloat(cell[i].style.left);
 }
 
+
 function moveRight()
 {
-	for(let i=cell.length-1;i>=0;--i)
-	{
-		let pos = 0;
-		cell[i].style.transitionDuration = desiredSpeed;
+	tapeBeginning = findBeginning();
+	tapeEnd = findEnd();
 
-		pos = parseFloat(cellpos[i]) - 11.11 + '%';
-		if(parseFloat(pos) < -11.11)
+	if(tapeEnd != 100)
+	{
+		for(let i=tapeEnd;i>=tapeBeginning;--i)
 		{
-			cell[i].style.transitionDuration = '0s';
-			pos = '99.99%';
+			let pos = parseFloat(cellpos[i]) - 11.11 + '%';
 			cell[i].style.left = pos;
-		} else {
-			cell[i].style.left = pos;
+			cellpos[i] = pos;
 		}
-		
-		cellpos[i] = pos;
+	} else {
+		//if initial cell in the tape is reached, alert the user
+		alert('Maximum side reached.');
 	}
 }
 
 function moveLeft()
 {
-	
-	for(let i=0;i<cell.length;++i)
+	tapeBeginning = findBeginning();
+	tapeEnd = findEnd();
+	//console.log(tapeEnd);
+
+	if(tapeEnd != 0)
 	{
-		let pos = 0;
-		cell[i].style.transitionDuration = desiredSpeed;
-
-		pos = parseFloat(cellpos[i]) + 11.11 + '%';
-		if(parseFloat(pos) > 99.99)
+		for(let i=tapeBeginning;i<tapeEnd;++i)
 		{
-			cell[i].style.transitionDuration = '0s';
-			pos = '-11.11%';
+			let pos = parseFloat(cellpos[i]) + 11.11 + '%';
 			cell[i].style.left = pos;
-		} else {
-			cell[i].style.left = pos;
+			cellpos[i] = pos;
 		}
-
-		cellpos[i] = pos;
+	} else {
+		//if final cell in the tape is reached, alert the user
+		alert('Maximum side reached.');
 	}
 }
 
+//reset cell positions to default
 function resetPos()
 {
 	for(let i=0;i<cell.length;++i)
@@ -76,12 +101,46 @@ function resetPos()
 	}
 }
 
+//finds cell one position before viewable cells on the right side
+function findBeginning()
+{
+	let startCell = 0;
+
+	//get cell leftmost on the tape
+	do
+	{
+		startCell++;
+	}
+	while(cell[startCell].style.left == '-11.11%');
+
+	//set startCell to one position behind previous result
+	startCell -= 1;
+
+	return startCell;
+}
+
+//finds cell one position before viewable cells on the left side
+function findEnd()
+{
+	let endCell = 100;
+	//get cell rightmost on the tape
+	while(cell[endCell].style.left == '99.99%')
+	{
+		endCell--;
+	}
+
+	//set startCell to one position behind previous result
+	endCell += 1;
+
+	return endCell;
+}
+
+//get user-selected speed from slider
 speedSelector.oninput = function() {
 	var speedSelectorValue = (parseInt(this.value) / 100);
 	for(let i=0;i<cell.length;i++)
 	{
 		cell[i].style.transitionDuration = speedSelectorValue + 's';
 		desiredSpeed = speedSelectorValue + 's';
-		//console.log(desiredSpeed);
 	}
 }
--- www/scripts/mapper.js
+++ www/scripts/mapper.js
@@ -1,83 +1,63 @@
-var cell = [document.getElementById('0'),
-			document.getElementById('1'),
-			document.getElementById('2'),
-			document.getElementById('3'),
-			document.getElementById('4'),
-			document.getElementById('5'),
-			document.getElementById('6'),
-			document.getElementById('7'),
-			document.getElementById('8'),
-			document.getElementById('9'),
-			document.getElementById('10')];
+//define all existing cells
+var cell = [document.getElementById('0'),document.getElementById('1'),document.getElementById('2'),
+			document.getElementById('3'),document.getElementById('4'),document.getElementById('5'),
+			document.getElementById('6'),document.getElementById('7'),document.getElementById('8'),
+			document.getElementById('9'),document.getElementById('10'),document.getElementById('11'),
+			document.getElementById('12'),document.getElementById('13'),document.getElementById('14'),
+			document.getElementById('15'),document.getElementById('16'),document.getElementById('17'),
+			document.getElementById('18'),document.getElementById('19'),document.getElementById('20'),
+			document.getElementById('21'),document.getElementById('22'),document.getElementById('23'),
+			document.getElementById('24'),document.getElementById('25'),document.getElementById('26'),
+			document.getElementById('27'),document.getElementById('28'),document.getElementById('29'),
+			document.getElementById('30'),document.getElementById('31'),document.getElementById('32'),
+			document.getElementById('33'),document.getElementById('34'),document.getElementById('35'),
+			document.getElementById('36'),document.getElementById('37'),document.getElementById('38'),
+			document.getElementById('39'),document.getElementById('40'),document.getElementById('41'),
+			document.getElementById('42'),document.getElementById('43'),document.getElementById('44'),
+			document.getElementById('45'),document.getElementById('46'),document.getElementById('47'),
+			document.getElementById('48'),document.getElementById('49'),document.getElementById('50'),
+				document.getElementById('51'),document.getElementById('52'),
+			document.getElementById('53'),document.getElementById('54'),document.getElementById('55'),
+			document.getElementById('56'),document.getElementById('57'),document.getElementById('58'),
+			document.getElementById('59'),document.getElementById('60'),document.getElementById('61'),
+			document.getElementById('62'),document.getElementById('63'),document.getElementById('64'),
+			document.getElementById('65'),document.getElementById('66'),document.getElementById('67'),
+			document.getElementById('68'),document.getElementById('69'),document.getElementById('70'),
+			document.getElementById('71'),document.getElementById('72'),document.getElementById('73'),
+			document.getElementById('74'),document.getElementById('75'),document.getElementById('76'),
+			document.getElementById('77'),document.getElementById('78'),document.getElementById('79'),
+			document.getElementById('80'),document.getElementById('81'),document.getElementById('82'),
+			document.getElementById('83'),document.getElementById('84'),document.getElementById('85'),
+			document.getElementById('86'),document.getElementById('87'),document.getElementById('88'),
+			document.getElementById('89'),document.getElementById('90'),document.getElementById('91'),
+			document.getElementById('92'),document.getElementById('93'),document.getElementById('94'),
+			document.getElementById('95'),document.getElementById('96'),document.getElementById('97'),
+			document.getElementById('98'),document.getElementById('99'),document.getElementById('100')
+			];
 
-var viewedValues = ['','','','','','','','','','',''];
-var tapeValues = [];
 var tapeInput;
-var beginningOfTape;
-var tapeValuesOffset = 0;
-
 var inputButton = document.getElementById('enterInput');
-document.getElementById('moverightbtn').addEventListener('click', function(){tapeValuesOffset++;calculateViewableTapeValues();});
-document.getElementById('moveleftbtn').addEventListener('click', function(){tapeValuesOffset--;calculateViewableTapeValues();});
-document.getElementById('resetposbtn').addEventListener('click', function(){tapeValuesOffset = 0;calculateViewableTapeValues();});
+//get current beginning of viewable cells
+var tapeBeginning = findBeginning();
 
 inputButton.onclick = function() {
-	//add check to see if input already exists or keep it like this
-	tapeValues.splice("",tapeValues.length);
+	//reset cells to default positions
 	resetPos();
-	let inputIndex = 0;
+	//reset beginning of tape var
+	tapeBeginning = findBeginning()
+	//clear contents from cells
+	for(let i=0;i<cell.length;++i)
+	{
+		cell[i].querySelector('.cell-contents').innerHTML = '';
+	}
+	//get values inputted from user
 	tapeInput = document.getElementById('tapeInput').value;
-
-	for(let i=0;i<tapeInput.length;++i)
+	//write values from input to cells from middle cell
+	for(let i=tapeBeginning+5;i<cell.length;++i)
 	{
-		if(i + 5 < viewedValues.length)
+		if(tapeInput[i-(tapeBeginning+5)] != undefined)
 		{
-			viewedValues[i+5] = tapeInput[i];
-		}
-		tapeValues.push(tapeInput[i]);
-	}
-
-	mapViewedTapeValues();
-}
-
-function mapViewedTapeValues() 
-{
-	beginningOfTape = getBeginningOfTape();
-	let currentCell = beginningOfTape;
-
-	//map values to tape
-	for(let i=0;i<viewedValues.length;++i)
-	{
-		cell[i].childNodes[1].innerHTML = viewedValues[i];
-	}
-}
-
-function getBeginningOfTape()
-{
-	let winner = '999%';
-	let leftmostCell;
-	for(let i=0;i<11;++i)
-	{
-		if(parseFloat(cell[i].style.left) < parseFloat(winner))
-		{
-			winner = cell[i].style.left;
-			leftmostCell = i;
+			cell[i].querySelector('.cell-contents').innerHTML = tapeInput[i-(tapeBeginning+5)];
 		}
 	}
-
-	//console.log(leftmostCell);
-
-	return leftmostCell;
-}
-
-function calculateViewableTapeValues()
-{
-
-	//for(let i=0;i<tapeValues.length;++i)
-	//{
-	//	viewedValues[(i + 5 - (tapeValuesOffset))] = tapeInput[i];
-	//}
-
-	//console.log(viewedValues);
-	//mapViewedTapeValues();
 }
